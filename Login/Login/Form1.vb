443533828d5810d2efbd8b9b13baea2bf7ea2c3c
proyectoinf <31289003+proyectoinf@users.noreply.github.com>
2017-09-15 15:27:11 -0500

    prueba

---
+++ Login/Login/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "proyectoinf" And TextBox2.Text = "prueba" Then
+            MsgBox("Login Correcto", MsgBoxStyle.Information)
+        Else
+            MsgBox("Login Incorrecto", MsgBoxStyle.Critical)
+        End If
+    End Sub
+End Class
